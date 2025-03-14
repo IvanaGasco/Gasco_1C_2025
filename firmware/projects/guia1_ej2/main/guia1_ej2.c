ce16e87d12aaf80231fd5cf458402456bb07b8e8
IvanaGasco <ivanagasco1@gmail.com>
2025-03-14 17:55:31 -0300

    Ejercicios 1 y 2 TP1

---
+++ firmware/projects/guia1_ej2/main/guia1_ej2.c
@@ -0,0 +1,100 @@
+/*! @mainpage Blinking switch
+ *
+ * \section genDesc General Description
+ *
+ * This example makes LED_1 and LED_2 blink if SWITCH_1 or SWITCH_2 are pressed.
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 12/09/2023 | Document creation		                         |
+ *
+ * @author Albano Peñalva (albano.penalva@uner.edu.ar)
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "led.h"
+#include "switch.h"
+/*==================[macros and definitions]=================================*/
+#define CONFIG_BLINK_PERIOD 100
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+/*
+void app_main(void){
+    uint8_t teclas;
+    LedsInit();
+    SwitchesInit();
+
+    while(1) {
+        teclas = SwitchesRead();
+
+        if (teclas == 0){
+            LedOff(LED_1);
+            LedOff(LED_2);
+            LedOff(LED_3);
+        }
+
+        // Si se presiona SWITCH_1, titila LED_1
+        if (teclas & SWITCH_1) {
+            LedToggle(LED_1);
+        }
+        else {
+
+        }
+
+
+        // Si se presiona SWITCH_2, titila LED_2
+        if (teclas & SWITCH_2) {
+            LedToggle(LED_2);
+        }
+
+        // Si ambas teclas están presionadas simultáneamente, titila LED_3
+        if ((teclas & SWITCH_1) && (teclas & SWITCH_2)) {
+            LedOff(LED_1);
+            LedOff(LED_2);
+            LedToggle(LED_3);
+        }
+
+        vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+    }
+}
+*/
+
+void app_main(void){
+	uint8_t teclas;
+	LedsInit();
+	SwitchesInit();
+    while(1)    {
+    	teclas  = SwitchesRead();
+    	switch(teclas){
+    		case SWITCH_1:
+                LedOff(LED_2);
+                LedOff(LED_3);
+    			LedToggle(LED_1);
+    		break;
+    		case SWITCH_2:
+                LedOff(LED_3);
+                LedOff(LED_1);
+    			LedToggle(LED_2);
+    		break;
+			case SWITCH_1 | SWITCH_2:
+                LedOff(LED_1);
+                LedOff(LED_2);
+                LedToggle(LED_3);
+    	}
+	    //LedToggle(LED_3);
+		vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+	}
+}
+
+
